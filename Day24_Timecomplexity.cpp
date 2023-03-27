f2649f02fd7a8aff155cc6d852b30f41a13e9090
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-03-27 14:28:09 +0500

    We solved prime number function in C++

--- Day24_Timecomplexity.cpp
+++ Day24_Timecomplexity.cpp
@@ -5,15 +5,15 @@
 #include <algorithm>
 using namespace std;
 
-bool isPrime(int n){
+bool isPrime(int num){
     bool is_prime = true;
     // 0 and 1 are not prime numbers
-    if (n == 0 || n == 1) {
+    if (num == 0 || num == 1) {
     is_prime = false;
     }
 
-    for (int i = 2; i <= n / 2; ++i) {
-        if (n % i == 0) {
+    for (int i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
             is_prime = false;
             break;
         }
