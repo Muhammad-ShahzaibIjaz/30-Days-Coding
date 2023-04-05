ede42862747ecd06110fa619bb65bcdb90345d31
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-06 00:37:05 +0500

    Correction

--- classes.cpp
+++ classes.cpp
@@ -64,17 +64,17 @@ int main() {
     
     cin >> age >> first_name >> last_name >> standard;
     
-    Student st;
-    st.set_age(age);
-    st.set_standard(standard);
-    st.set_first_name(first_name);
-    st.set_last_name(last_name);
+    Student st1;
+    st1.set_age(age);
+    st1.set_standard(standard);
+    st1.set_first_name(first_name);
+    st1.set_last_name(last_name);
     
-    cout << st.get_age() << "\n";
-    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
-    cout << st.get_standard() << "\n";
+    cout << st1.get_age() << "\n";
+    cout << st1.get_last_name() << ", " << st1.get_first_name() << "\n";
+    cout << st1.get_standard() << "\n";
     cout << "\n";
-    cout << st.to_string();
+    cout << st1.to_string();
     
     return 0;
 }
