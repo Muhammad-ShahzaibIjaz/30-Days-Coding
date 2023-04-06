cb849407d00259333e8ed56bbff536d045571ee7
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-07 01:57:19 +0500

    Updation

--- classesAndObject.cpp
+++ classesAndObject.cpp
@@ -28,19 +28,19 @@ class Student{
 int main() {
     int n; // number of students
     cin >> n;
-    Student *s = new Student[n]; // an array of n students
+    Student *s1 = new Student[n]; // an array of n students
     
     for(int i = 0; i < n; i++){
-        s[i].input();
+        s1[i].input();
     }
 
     // calculate kristen's score
-    int kristen_score = s[0].calculateTotalScore();
+    int kristen_score = s1[0].calculateTotalScore();
 
     // determine how many students scored higher than kristen
     int count = 0; 
     for(int i = 1; i < n; i++){
-        int total = s[i].calculateTotalScore();
+        int total = s1[i].calculateTotalScore();
         if(total > kristen_score){
             count++;
         }
