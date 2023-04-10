a973e1e11e691626920efee86eec146cd1d73e0d
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-11 01:30:00 +0500

    Updation

--- Vector-Erase.cpp
+++ Vector-Erase.cpp
@@ -10,23 +10,23 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
     cin >> n;
-    vector<int> v(n);
+    vector<int> v1(n);
     for (int i = 0; i < n; i++) {
-        cin >> v[i];
+        cin >> v1[i];
     }
     int pos, start, end;
     cin >> pos >> start >> end;
 
     // remove element at given position
-    v.erase(v.begin() + pos - 1);
+    v1.erase(v1.begin() + pos - 1);
 
     // remove range of elements
-    v.erase(v.begin() + start - 1, v.begin() + end - 1);
+    v1.erase(v1.begin() + start - 1, v1.begin() + end - 1);
 
     // print size of vector and remaining elements
-    cout << v.size() << endl;
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << " ";
+    cout << v1.size() << endl;
+    for (int i = 0; i < v1.size(); i++) {
+        cout << v1[i] << " ";
     }
     cout << endl;
     return 0;
