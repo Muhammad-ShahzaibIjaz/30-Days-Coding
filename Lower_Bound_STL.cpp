87e39257ae6b8eef28a26ac167074690bac8febf
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-13 00:49:59 +0500

    Lower_Bound_STL Code Updation

--- Lower_Bound_STL.cpp
+++ Lower_Bound_STL.cpp
@@ -8,19 +8,19 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n, q;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++) {
+    int N, query;
+    cin >> N;
+    int a[N];
+    for (int i = 0; i < N; i++) {
         cin >> a[i];
     }
-    cin >> q;
-    int x[q];
-    for (int i = 0; i < q; i++) {
+    cin >> query;
+    int x[query];
+    for (int i = 0; i < query; i++) {
         cin >> x[i];
     }
-    for (int i = 0; i < q; i++) {
-        int* p = lower_bound(a, a + n, x[i]);
+    for (int i = 0; i < query; i++) {
+        int* p = lower_bound(a, a + N, x[i]);
         if (*p == x[i]) {
             cout << "Yes " << (p - a + 1) << endl;
         } else {
