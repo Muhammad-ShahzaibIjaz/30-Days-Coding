f44a8e0ae4a5a93f4de4cc898cda352f506e0280
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-17 00:03:59 +0500

    SET-STL problem

---
+++ set-stl.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int q;
+    cin >> q;
+
+    set<int> s;
+
+    while (q--) {
+        int type, x;
+        cin >> type >> x;
+
+        if (type == 1) {
+            s.insert(x);
+        } else if (type == 2) {
+            s.erase(x);
+        } else {
+            if (s.count(x)) {
+                cout << "Yes\n";
+            } else {
+                cout << "No\n";
+            }
+        }
+    }  
+    return 0;
+}
+
+
+
