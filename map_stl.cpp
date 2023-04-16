d3bb04e15bb1091dcbe7081303c433c7647e9d35
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-16 18:55:12 +0500

    Map_stl problem solved

---
+++ map_stl.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+
+    map<string, int> marks;
+
+    for (int i = 0; i < n; i++) {
+        int query;
+        string name;
+        cin >> query >> name;
+
+        if (query == 1) {
+            int mark;
+            cin >> mark;
+            marks[name] += mark;
+        } else if (query == 2) {
+            marks.erase(name);
+        } else {
+            if (marks.count(name)) {
+                cout << marks[name] << endl;
+            } else {
+                cout << "0" << endl;
+            }
+        }
+    }  
+    return 0;
+}
+
+
+
