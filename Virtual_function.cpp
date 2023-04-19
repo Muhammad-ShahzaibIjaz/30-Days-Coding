1ea2db0d1886063cd4dd189e561c56d184616e1d
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-19 09:04:26 +0500

    Updation

--- Virtual_function.cpp
+++ Virtual_function.cpp
@@ -65,24 +65,24 @@ int main(){
 
     int n, val;
     cin>>n; //The number of objects that is going to be created.
-    Person *per[n];
+    Person *person[n];
 
     for(int i = 0;i < n;i++){
 
         cin>>val;
         if(val == 1){
             // If val is 1 current object is of type Professor
-            per[i] = new Professor;
+            person[i] = new Professor;
 
         }
-        else per[i] = new Student; // Else the current object is of type Student
+        else person[i] = new Student; // Else the current object is of type Student
 
-        per[i]->getdata(); // Get the data from the user.
+        person[i]->getdata(); // Get the data from the user.
 
     }
 
     for(int i=0;i<n;i++)
-        per[i]->putdata(); // Print the required output for each object.
+        person[i]->putdata(); // Print the required output for each object.
 
     return 0;
 
