316c4016da8a1c5c9f0f602d75f43898b5fb2b77
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-23 23:20:18 +0500

    Sum two problem of leet code

---
+++ SumTwo.cpp
@@ -0,0 +1,11 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hashmap.count(complement)) {
+                return {hashmap[complement], i};
+            }
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
