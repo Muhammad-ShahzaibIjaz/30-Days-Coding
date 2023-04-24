9bfe48773824d2267382542f796cb8af88f49a61
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-24 10:39:39 +0500

    Add two number by leetcode

---
+++ Add_Two_Number.cpp
@@ -0,0 +1,22 @@
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int carry = 0;
+    ListNode* result = new ListNode(0);
+    ListNode* current = result;
+    
+    while (l1 || l2) {
+        int val1 = l1 ? l1->val : 0;
+        int val2 = l2 ? l2->val : 0;
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        current->next = new ListNode(sum % 10);
+        current = current->next;
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
+    }
+    
+    if (carry) {
+        current->next = new ListNode(carry);
+    }
+    
+    return result->next;
+}
