0adfa94253e8d96a3273e312b83073caf6f75dc5
Muhammad Shahzaib Ijaz <muhammadshahzaibijaz34@gmail.com>
2023-04-25 20:51:28 +0500

    Longest Substring

---
+++ LongestSubstring.cpp
@@ -0,0 +1,14 @@
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    set<char> st;
+    int ans = 0, i = 0, j = 0;
+    while (i < n && j < n) { 
+        if (!st.count(s[j])) { 
+            st.insert(s[j++]); 
+            ans = max(ans, j - i); 
+        } else { 
+            st.erase(s[i++]); 
+        } 
+    } 
+    return ans;
+}
